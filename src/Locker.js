85e21261f4de0d3adb294b11e1e960ae546e420d
Tzach Yarimi <tzachyrm@gmail.com>
2021-12-06 11:24:31 +0200

    .

---
+++ src/Locker.js
@@ -0,0 +1,7 @@
+import styles from "./Locker.module.css";
+
+const Locker = ({ pinCode, onUnlock }) => {
+    return <div className={styles.locker}>{"I'm going to be a locker!!"}</div>;
+};
+
+export default Locker;
